7493305e3314731f00123b1ebbf06bc62cabe94c
Matt Delaney <mcdelaney@gmail.com>
2020-04-16 12:03:21 -0600

    collect args

--- R/DbConn.R
+++ R/DbConn.R
@@ -35,9 +35,10 @@ DbConn = R6Class(
                           dataset=NA) {
       self$log <- logging::getLogger('DbConn')
       logging::setLevel('INFO')
-      self$dataset <- dataset
-      self$database <- database
-      self$cred_location <- cred_location
+      args <- as.list(match.call(expand.dots=T))
+      self$dataset <- args$dataset
+      self$database <- args$database
+      self$cred_location <- args$cred_location
       self$creds <- mmkit::read_creds(database, cred_location=self$cred_location)
       
       self$log$info("Creating connection to %s.%s", database, self$creds$db_name)
